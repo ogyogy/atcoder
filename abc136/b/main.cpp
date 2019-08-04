0e12926a1eacbfacf74a9d8cec66ffadafd1fb9e
ogyogy <47146269+ogyogy@users.noreply.github.com>
2019-08-04 22:32:05 +0900

    abc136を追加

---
+++ abc136/b/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define REP(i, n) for (ll i = 0; i < (n); i++)
+#define REPR(i, n) for (ll i = (n); i >= 0; i--)
+#define FOR(i, m, n) for (ll i = (m); i < (n); i++)
+#define FORR(i, m, n) for (ll i = (m); i >= (n); i--)
+#define INF 1e9
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((ll)(x).size())
+#define SORT(x) sort((x).begin(), (x).end())
+#define dump(x) cerr << #x << " = " << (x) << endl
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" \
+                      << " " << __FILE__ << endl
+
+int main(int argc, char const *argv[])
+{
+    ll n;
+    cin >> n;
+    ll res = 0;
+    FOR(i, 1, n + 1)
+    {
+        ostringstream oss;
+        oss << i;
+        string s = oss.str();
+        if ((ll)s.length() % 2 != 0)
+            res++;
+    }
+    cout << res << endl;
+    return 0;
+}
